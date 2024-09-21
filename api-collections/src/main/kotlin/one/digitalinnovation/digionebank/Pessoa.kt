c8ead74f65f9cff0d9f1d0ca3e64103feb35be25
thalesbezerra <bezerra.thales@gmail.com>
2024-09-21 20:48:00 -0300

    Modificando o acesso as variáveis com o (private set)

--- api-collections/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,27 +2,26 @@ package one.digitalinnovation.digionebank
 
 class Pessoa{
     var nome: String = "Jether"
+
     var cpf: String = "123.123.123-11"
-        private set
+    private set // Não é mais possivel trocar o cpf que é o logico
 
-    /*Criando uma class inner para representar o endereço dentro da class
-    Pessos.
-    Estudar sobre inner no chatgpt
 
-     =============
+//    Criando uma class inner para representar o endereço dentro da class Pessos.
+//
+//    inner class Endereco {
+//        var rua: String = "Rua test"
+//    }
 
-    inner class Endereco {
-        var rua: String = "Rua test"
-    }
-
-     */
 }
 
 
 fun main(){
     val jether = Pessoa()
+
     println(jether.nome)
     println(jether.cpf)
 
-    //println(jether.Endereco().rua)
+//    println(jether.Endereco().rua)
+
 }
